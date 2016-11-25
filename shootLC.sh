8fc56fd464dd42734f91089d99f9bc7b00676821
ddhee <kidakitaki@gmail.com>
2016-11-25 11:28:18 +0800

    nothing special

---
+++ shootLC.sh
@@ -0,0 +1,5 @@
+ml -c shootLC.asm
+link16 shootLC.obj,shootLC.exe,nul.map,.lib,nul.def
+dosbox shootLC.exe
+
+
--- shootexp.asm
+++ shootexp.asm
@@ -16,10 +16,22 @@ prompt_t     db "TIME: ",'$'
 ptime db ?, ?, ?  ;hours, minutes, seconds
 ctime db ?, ?, ?  ;hours, minutes, seconds
 time  db ?        ;in minutes
-rpg   db ?        ;position of RPG (10-68, taking up 3 characters)
+rpg   db ?        ;position of RPG (10-68, taking up 3 pixels)
 score db ?
-bugs  db 4 dup(60 dup(?))
-grnds db 10 dup(58 dup(?))
+bugs  db 60 dup(?)
+      db 60 dup(?)
+      db 60 dup(?)
+      db 60 dup(?)
+grnds db 58 dup(?)
+      db 58 dup(?)
+      db 58 dup(?)
+      db 58 dup(?)
+      db 58 dup(?)
+      db 58 dup(?)
+      db 58 dup(?)
+      db 58 dup(?)
+      db 58 dup(?)
+      db 58 dup(?)
 
 
 ;-----------------------------------------------
@@ -35,33 +47,28 @@ local reset_bugs, reset_grnds, again1, again2
   mov [di+2], dl
   mov score, 0
   lea di, bugs
-  mov cx, 4        
-  ;4 rows of bugs
-  reset_bugs:
-    mov ax, 60       
-    ;60 bugs for each row
-    again1:
-      mov dl, 0
-      mov [di], dl
-      inc di
-      dec ax
-      jnz again1
-    loop reset_bugs
-    lea di, grnds
-    mov cx, 10       
-  ;10 rows of grenades
-  reset_grnds:
-    mov ax, 58       
-    ;58 grenades for each row
-    again2:
-      mov dl, 0
-      mov [di], dl
-      inc di
-      dec ax
-      jnz again2
-    loop reset_grnds
-  ;set rpg at the center of the track
-  set_rpg 39
+  mov cx, 4        ;4 rows of bugs
+reset_bugs:
+  mov ax, 60       ;60 bugs for each row
+  again1:
+    mov dl, 0
+    mov [di], dl
+    inc di
+    dec ax
+    jnz again1
+  loop reset_bugs
+  lea di, grnds
+  mov cx, 10       ;10 rows of grenades
+reset_grnds:
+  mov ax, 58       ;58 grenades for each row
+  again2:
+    mov dl, 0
+    mov [di], dl
+    inc di
+    dec ax
+    jnz again2
+  loop reset_grnds
+  set_rpg 39       ;set rpg at the center of the track
   refresh
 endm
 
@@ -107,13 +114,9 @@ endm
 draw macro char
   push ax
   push dx
-  push cx
-  push bx
-  mov ah, 02h
+  mov ah, 02
   mov dl, char
   int 21h
-  pop bx
-  pop cx
   pop dx
   pop ax
 endm 
@@ -143,7 +146,7 @@ endm
 
 ;refresh view
 refresh macro
-local draw_bugs, draw_grnds, grndgrnds, bugbugs, no_bugs, no_grnds, grnds_over, bugs_over, bugs_body
+local draw_bugs, draw_grnds
   ;get time
   mov ah, 2ch
   int 21h
@@ -166,78 +169,23 @@ local draw_bugs, draw_grnds, grndgrnds, bugbugs, no_bugs, no_grnds, grnds_over, 
   sub ax, [di+2]
   mov time, al
   
-  cursor 1, 5
+  cursor 1, 1
   display prompt_s
   numeral score
   cursor 1, 60
   display prompt_t
   numeral time
 
-;draw grenades
-  lea si, grnds
-  mov bh, 10
-  mov bl, 3
-  grndgrnds:
-    mov cx, 58
-    cursor bl, 11
-    draw_grnds:
-      mov dl, [si]
-      cmp dl, 0
-      jz  no_grnds
-      draw 'o'
-      jmp grnds_over
-      no_grnds:
-        draw ' '
-      grnds_over:
-      inc si
-      loop draw_grnds
-    inc bl
-    dec bh
-    jnz grndgrnds
-
-;draw bugs
   lea si, bugs
-  mov bh, 4
-  mov bl, 3
-  bugbugs:
-    mov cx, 60
-    cursor bl, 10
-    draw_bugs:
-      mov dl, [si]
-      cmp dl, 0
-      jz  no_bugs
-      cmp dl, 1
-      jz  bug_body
-      draw 2
-      jmp bugs_over
-      bug_body:
-        draw 3
-        jmp bugs_over
-      no_bugs:
-        draw ' '
-      bugs_over:
-        inc si
-      loop draw_bugs
-    inc bl
-    dec bh
-    jnz bugbugs
-
-;draw RPG
-  cursor 21, rpg
-  draw '-'
-  mov bl, rpg
-  dec bl
-  cursor 21, bl
-  draw '.'
-  cursor 22, bl
-  draw '|'
-  inc bl
-  inc bl
-  cursor 21, bl 
-  draw '.'
-  cursor 22, bl
-  draw '|'
-
+  mov cx, 60
+  cursor 3, 10
+draw_bugs:
+  cmp [si], 0
+  jnz  no_bugs
+  draw 2
+  no_bugs:
+  inc si
+  loop draw_bugs
 
 endm
 
@@ -270,38 +218,116 @@ main proc far
   mov ax, @data
   mov ds, ax
 
-  mov ah, 0
-  mov al, 03h
-  int 10h
   clr_scr
   cursor 15, 0
   display string_hints
 
   ;wait for operation
-  waittostart:
-    mov ah, 01
-    int 16h
-    jz  waittostart
-    mov ah, 0
-    int 16h
-    cmp al, 'Q'
-    je  exit
-    cmp al, 'q'
-    je  exit
-    cmp al, 'S'
-    je  starto
-    cmp al, 's'
-    je  starto
-    jmp waittostart
+waittostart:
+  mov ah, 01
+  int 16h
+  jz  waittostart
+  mov ah, 0
+  int 16h
+  cmp al, 'Q'
+  je  exit
+  cmp al, 'q'
+  je  exit
+  cmp al, 'S'
+  je  starto
+  cmp al, 's'
+  je  starto
+  jmp waittostart
 
-  starto:
-    init
-    
+starto:
+    clr_scr
+  mov ah, 2ch
+  int 21h
+  lea di, ptime
+  mov [di], ch
+  mov [di+1], cl
+  mov [di+2], dl
+  mov score, 0
+  lea di, bugs
+  mov cx, 4        ;4 rows of bugs
+reset_bugs:
+  mov ax, 60       ;60 bugs for each row
+  again1:
+    mov dl, 0
+    mov [di], dl
+    inc di
+    dec ax
+    jnz again1
+  loop reset_bugs
+  lea di, grnds
+  mov cx, 10       ;10 rows of grenades
+reset_grnds:
+  mov ax, 58       ;58 grenades for each row
+  again2:
+    mov dl, 0
+    mov [di], dl
+    inc di
+    dec ax
+    jnz again2
+  loop reset_grnds
+  set_rpg 39  
+  mov ah, 2ch
+  int 21h
+  lea di, ctime
+  mov [di], ch
+  mov [di+1], cl
+  mov [di+2], dl
+  mov ax, 0
+  lea si, ptime
+  lea di, ctime
+  add ax, [si]
+  sub ax, [di]
+  mov bl, 60
+  mul bl
+  add ax, [si+1]
+  sub ax, [di+1]
+  mov bl, 60
+  mul bl
+  add ax, [si+2]
+  sub ax, [di+2]
+  mov time, al
+  
+  cursor 1, 1
+  display prompt_s
+  numeral score
+  cursor 1, 60
+  display prompt_t
+  numeral time
 
-  exit:
-    mov ax, 4c00h
-    int 21h
+  lea si, bugs
+  mov bl, 4
+  mov bh, 3
+bugbugs:
+  mov cx, 60
+  cursor bh, 10
+  push bh
+draw_bugs:
+  mov dx, [si]
+  cmp dx, 0
+  jz  no_bugs
+  draw 2
+  jmp bugs_over
+  no_bugs:
+    draw 2
+  bugs_over:
+  inc si
+  loop draw_bugs
+  pop 
+  inc bh
+  dec bl
+  jnz bugbugs
+
+exit:
+  mov ax, 4c00h
+  int 21h
 main endp
   end main
+
+
 
 
